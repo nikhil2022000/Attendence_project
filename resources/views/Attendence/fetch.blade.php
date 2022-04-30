963034808e370319aedbf1009788fe2d6b8b02cb
vipan kumar <vipannikhil2022000@gmail.com>
2022-04-30 13:32:59 +0530

    frontend degin

--- resources/views/Attendence/fetch.blade.php
+++ resources/views/Attendence/fetch.blade.php
@@ -2,43 +2,7 @@
 @section('main-container')
 
 <!-- Row-->
-<div class="row">
-	<div class="col-md-12">
-		<div class="card">
-			<div class="card-header  border-0">
-				<h4 class="card-title">Days Overview This Month</h4>
-			</div>
-			<div class="card-body pt-0 pb-3">
-				<div class="row mb-0 pb-0">
-					<div class="col-md-6 col-lg-2 text-center py-5">
-						<span class="avatar avatar-md bradius fs-20 bg-primary-transparent">31</span>
-						<h5 class="mb-0 mt-3">Total Working Days</h5>
-					</div>
-					<div class="col-md-6 col-lg-2 text-center py-5 ">
-						<span class="avatar avatar-md bradius fs-20 bg-success-transparent">24</span>
-						<h5 class="mb-0 mt-3">Present Days</h5>
-					</div>
-					<div class="col-md-6 col-lg-2 text-center py-5">
-						<span class="avatar avatar-md bradius fs-20 bg-danger-transparent">2</span>
-						<h5 class="mb-0 mt-3">Absent Days</h5>
-					</div>
-					<div class="col-md-6 col-lg-2 text-center py-5">
-						<span class="avatar avatar-md bradius fs-20 bg-warning-transparent">0</span>
-						<h5 class="mb-0 mt-3">Half Days</h5>
-					</div>
-					<div class="col-md-6 col-lg-2 text-center py-5 ">
-						<span class="avatar avatar-md bradius fs-20 bg-orange-transparent">2</span>
-						<h5 class="mb-0 mt-3">Late Days</h5>
-					</div>
-					<div class="col-md-6 col-lg-2 text-center py-5">
-						<span class="avatar avatar-md bradius fs-20 bg-pink-transparent">5</span>
-						<h5 class="mb-0 mt-3">Holidays</h5>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+
 <!-- End Row-->
 
 <!-- Row -->
