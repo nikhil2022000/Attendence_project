94b929263f62abec2d13cb11f23cd18f5e9f79f9
vipan kumar <vipannikhil2022000@gmail.com>
2022-05-19 17:21:19 +0530

    style set

--- resources/views/Attendence/import.blade.php
+++ resources/views/Attendence/import.blade.php
@@ -11,9 +11,15 @@
 									</div>
 									<div class="card-footer  text-white z-index-10" style="background-color: #917d2b;">
                                     <form  method="post" enctype="multipart/form-data" action="{{url('import')}}"> 
+									<div class="row">
+										<div class="col-md-4">
 												<div class="modal-body">
                                                   {{ csrf_field() }}
                                                             <input type="file" name="file">
+														</div>
+													</div>
+													<div class="col-md-4">
+														<div class="modal-body">		
                                                             <select style="padding: 4px;width: 151px;" data-placeholder="Select Month" name="month">
 														<option label="Select Month"></option>
 														<option value="January">January</option>
@@ -29,7 +35,11 @@
 														<option value="November">November</option>
 														<option value="December">December</option>
 													</select>
-													<select style="padding: 4px;width: 151px; margin-left: 131px;" data-placeholder="Select year" name="year">
+														</div>
+													</div>
+													<div class="col-md-4">	
+													<div class="modal-body">
+													<select style="padding: 4px;width: 151px;" data-placeholder="Select year" name="year">
 														<option label="Select year"></option>
 														<option value="2020">2020</option>
 														<option value="2021">2021</option>
@@ -37,7 +47,11 @@
 														<option value="2023">2023</option>
 														<option value="2024">2024</option>
 													</select>
-                                                           </div>
+
+													</div>
+
+                                                </div>
+                                                </div>
 														   
                                                   <div class="modal-footer">
 												<button type="submit" class="btn btn-primary">submit</button>
